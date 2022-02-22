1f38d54723ee83e9e0c5f4fbc3a4addeb1bd91de
Boris <34108415+Bodrian@users.noreply.github.com>
2022-02-23 00:04:34 +0300

    task1 - ver.2
    Обязательное задание. Переписал согласно рекомендациям скрипт.

--- design/lesson3.sql
+++ design/lesson3.sql
@@ -1,31 +1,45 @@
-create table if not exists Жанры (
-	Название varchar(40) primary key
+create table if not exists Genre (
+	Name_genre varchar(40) primary key
 );
 
-create table if not exists Исполнитель (
-	Идентификатор serial primary key,
-	Имя varchar(40) not null,
-	Жанр varchar(40) references Жанры (Название)
+create table if not exists Name_artist (
+	Id serial primary key,
+	Name_artist varchar(40) not null
 );
 
-create table if not exists Альбом (
-	Идентификатор serial primary key,
-	Название_альбома varchar(40) not null,
-	Год_выпуска integer not null check (Год_выпуска > 1900 and Год_выпуска < 2023),
-	Идентификатор_исполнителя integer references Исполнитель (Идентификатор)
+create table if not exists Album_info (
+	Id serial primary key,
+	Album_name varchar(40) not null,
+	Year integer not null check (Year > 1900 and Year < 2023)
 );
 
-create table if not exists Треки (
-	Идентификатор serial primary key,
-	Название_трека varchar(40) not null,
-	Длительность integer not null,
-	Идентификатор_альбома integer unique references Альбом (Идентификатор)
+create table if not exists Collection_info (
+	Id serial primary key,
+	Collection_name varchar(40) not null,
+	Year integer not null check (Year > 1900 and Year < 2023)
 );
 
-create table if not exists Сборник (
-	Идентификатор serial primary key,
-	Название_сборника varchar(40) not null,
-	Год_выпуска integer not null check (Год_выпуска > 1900 and Год_выпуска < 2023),
-	Идентификатор_трека integer references Треки (Идентификатор)
-	Идентификатор_альбома integer references Альбом (Идентификатор)
+create table if not exists Artist (
+	Id serial primary key,
+	Artist_name_Id integer references Name_artist (Id),
+	Genre varchar(40) references Genre (Name_genre)
+);
+
+create table if not exists Album (
+	Id serial primary key,
+	Album_info_Id integer references Album_info (Id),
+	Artist_Id integer references Artist (Id)
+);
+
+create table if not exists Track (
+	Id serial primary key,
+	Track_name varchar(40) not null,
+	Duration integer not null,
+	Album_Id integer references Album (Id)
+);
+
+create table if not exists Collection (
+	Collection_info_Id integer references Collection_info (Id),
+	Track_Id integer references Track (Id),
+	constraint pk primary key (Collection_info_Id, Track_Id)
 );
